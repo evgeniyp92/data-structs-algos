2a5146b36830e9944bedaf7266aea18bfb95250c
Evgeniy Pimenov <contact@evgeniypimenov.com>
2024-03-22 12:57:05 +0100

    alternate anagram solution

--- 7-anagrams/index.js
+++ 7-anagrams/index.js
@@ -52,10 +52,38 @@ function anagrams(stringA, stringB) {
 	return true
 }
 
-function anagramsAlt(stringA, stringB) {
-	function generateCharMap(string) {
+// ------------------------------------------------------------------------------------------------------------------ //
 
+function anagramsAlt(stringA, stringB) {
+	// build character maps for both strings, removing special symbols and spaces as well
+	const aCharMap = buildCharMap(stringA);
+	const bCharMap = buildCharMap(stringB);
+
+	// convert both charmaps into arrays of keys and check their length, mismatch = not an anagram
+	const aKeys = Object.keys(aCharMap)
+	const bKeys = Object.keys(bCharMap)
+	if (aKeys.length !== bKeys.length) return false
+
+	// iterate over one charmap and check if the value matches on the other one, if it doesnt match its not an anagram
+	for (const char in aCharMap) {
+		if (aCharMap[char] !== bCharMap[char]) return false
 	}
+
+	// all checks exhausted, return true
+	return true
+}
+
+function buildCharMap(string) {
+	const seen = {}
+	const cleanString = string.replace(/[^\w]g/, '').toLowerCase()
+	for (const char of cleanString) {
+		if (seen[char]) {
+			seen[char]++
+		} else {
+			seen[char] = 1
+		}
+	}
+	return seen
 }
 
 module.exports = anagrams;
