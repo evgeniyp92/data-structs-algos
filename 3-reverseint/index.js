8210855f91eef88b6dc3147bfc1c9cbbbb0169a5
Evgeniy Pimenov <contact@evgeniypimenov.com>
2024-03-22 09:59:18 +0100

    write alternate solution to reverseInt

--- 3-reverseint/index.js
+++ 3-reverseint/index.js
@@ -12,13 +12,21 @@ function reverseInt(n) {
 	if (n >= 0) {
 		let string = n.toString();
 		let reversed = string.split('').reverse().join('')
-		console.log(reversed)
-		return Number(reversed)
+		return parseInt(reversed)
 	} else {
 		let string = n.toString();
 		let reversed = '-' + string.split('-')[1].split('').reverse().join('')
-		return Number(reversed)
+		return parseInt(reversed)
 	}
+}
+
+function alternateReverseInt(n) {
+	const reversed = n.toString().split('').reverse().join('');
+	// if (n < 0) {
+	// 	return parseInt(reversed) * -1
+	// }
+	// return parseInt(reversed)
+	return parseInt(reversed) * Math.sign(n) // Math.sign() returns 1 or -1 depending if the value is positive or negative
 }
 
 module.exports = reverseInt;
