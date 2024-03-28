b0c9906db8c5551951acdce54a69bb3665b7ca5d
Evgeniy Pimenov <contact@evgeniypimenov.com>
2024-03-28 14:43:06 +0100

    write memoizer for fibonacci

--- 14-fib/index.js
+++ 14-fib/index.js
@@ -8,16 +8,41 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {
-    // O(2^n) complexity, 500-600ms to run fib(15)
-    // it doesnt exactly double, but it is a dramatic increase in function calls to calculate a value
-    // never do recursion unless you're specifically asked
+function slowFib(n) {
+/*
+    O(2^n) complexity, 500-600ms to run fib(15)
+    it doesnt exactly double, but it is a dramatic increase in function calls to calculate a value
+    never do recursion unless you're specifically asked
+*/
     if (n < 2) return n
-    return fib(n - 1) + fib(n - 2)
-    // this works because the returned function calls always work down to fib(0) or fib(1)
-    // so at the base level we just add zeroes and ones, and we work our way back up with bigger and bigger numbers
-    // until we get to the top and the result is known
+    return slowFib(n - 1) + slowFib(n - 2)
+/*
+    This works because the returned function calls always work down to fib(0) or fib(1)
+    so at the base level we just add zeroes and ones, and we work our way back up with bigger and bigger numbers
+    until we get to the top and the result is known
+*/
 }
+
+function memoize(fn) {
+    // make a record of all previous calls to the function
+    const cache = {};
+    // return an anonymous function with the args provided
+    return function(...args) {
+        // if we've seen this function called this way before
+        if (cache[args]) {
+            // return the result immediately
+            return cache[args]
+        }
+        // using apply to allow us to call fn with an array of args
+        const result = fn.apply(null, args)
+        // store the result
+        cache[args] = result;
+        // return it
+        return result;
+    }
+}
+
+const fib = memoize(slowFib)
 
 function fibIterative(n) {
     // O(n) time complexity, <1ms to run fib(15)
